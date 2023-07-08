087bfd1d6aee00ffe815995c36e341d39736b577
lakhi123 <46920567+lakhi123@users.noreply.github.com>
2023-07-08 15:32:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.5.2" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,7 +13,7 @@ provider "aws" {
         #profile = "default"
 	}
 
-resource "aws_instance" "Raminfra" {
-	ami = "ami-0c2b8ca1dad447f8a" 
+resource "aws_instance" "Priyankainfra" {
+	ami = "ami-06b09bfacae1453cb" 
 	instance_type = "t2.micro"
 }
